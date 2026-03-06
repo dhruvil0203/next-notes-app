2ef9f790dccfb059f85969067956a74c98f8799d
Dhruvil Mistry <dkmistry0203@gmail.com>
2026-03-07 00:24:08 +0530

    fix delete and put api

--- app/api/notes/[id]/route.js
+++ app/api/notes/[id]/route.js
@@ -2,48 +2,58 @@ import dbConnection from "@/lib/db";
 import Note from "@/models/notes";
 import { NextResponse } from "next/server";
 
-export async function DELETE({ params }) {
+export async function DELETE(request, { params }) {
   try {
-    const { id } = await params;
+    const { id } = params;
+
     await dbConnection();
+
     const note = await Note.findByIdAndDelete(id);
+
     if (!note) {
       return NextResponse.json(
         { success: false, error: "Note not found" },
-        { status: 404 },
+        { status: 404 }
       );
     }
+
     return NextResponse.json({ success: true, data: {} });
+
   } catch (error) {
     return NextResponse.json(
-      { success: false, error: message },
-      { status: 400 },
+      { success: false, error: error.message },
+      { status: 400 }
     );
   }
 }
 
 export async function PUT(request, { params }) {
   try {
-    const { id } = await params;
+    const { id } = params;
+
     await dbConnection();
-    const body = await request.json()
+
+    const body = await request.json();
+
     const note = await Note.findByIdAndUpdate(
       id,
       { ...body, updatedAt: Date.now() },
       { new: true, runValidators: true }
     );
+
     if (!note) {
       return NextResponse.json(
         { success: false, error: "Note not found" },
-        { status: 404 },
+        { status: 404 }
       );
     }
+
     return NextResponse.json({ success: true, data: note });
 
   } catch (error) {
     return NextResponse.json(
-      { success: false, error: message },
-      { status: 400 },
+      { success: false, error: error.message },
+      { status: 400 }
     );
   }
 }
