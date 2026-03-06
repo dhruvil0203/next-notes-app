69ddd75ef8aeed98bfb1abed7926fb8219fa610b
Dhruvil Mistry <dkmistry0203@gmail.com>
2026-03-06 23:55:43 +0530

    feat: complete Next.js notes app with MongoDB, REST API, and full CRUD functionality

--- app/api/notes/[id]/route.js
+++ app/api/notes/[id]/route.js
@@ -21,3 +21,29 @@ export async function DELETE({ params }) {
     );
   }
 }
+
+export async function PUT(request, { params }) {
+  try {
+    const { id } = await params;
+    await dbConnection();
+    const body = await request.json()
+    const note = await Note.findByIdAndUpdate(
+      id,
+      { ...body, updatedAt: Date.now() },
+      { new: true, runValidators: true }
+    );
+    if (!note) {
+      return NextResponse.json(
+        { success: false, error: "Note not found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json({ success: true, data: note });
+
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: message },
+      { status: 400 },
+    );
+  }
+}
--- components/NotesClient.jsx
+++ components/NotesClient.jsx
@@ -7,6 +7,9 @@ const NotesClient = ({ intialNotes }) => {
   const [title, setTitle] = useState("");
   const [loading, setLoading] = useState(false);
   const [content, setContent] = useState("");
+  const [editingId, setEditingId] = useState(null);
+  const [editTitle, setEditTitle] = useState("");
+  const [editContent, setEditContent] = useState("");
 
   const createNote = async (e) => {
     e.preventDefault();
@@ -50,6 +53,45 @@ const NotesClient = ({ intialNotes }) => {
     }
   };
 
+  const updateNode = async (id) => {
+    if (!editTitle.trim() || !editContent.trim()) return;
+    setLoading(true);
+    try {
+      const response = await fetch(`/api/notes/${id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ title: editTitle, content: editContent })
+        }
+      )
+      const result = await response.json();
+      if (result.success) {
+        setNotes(notes.map((note) => (note._id === id ? result.data : note)))
+        setEditingId(null);
+        setEditTitle("");
+        setEditContent("");
+        toast.success("Note updated successfully");
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log("Error updating note: ", error);
+      toast.error("Something went wrong.");
+      setLoading(false);
+    }
+  }
+
+  const startEdiditng = (note) => {
+    setEditingId(note._id);
+    setEditTitle("");
+    setEditContent("");
+  }
+
+  const cancelEditing = () => {
+    setEditingId(null);
+    setEditTitle("");
+    setEditContent("");
+  }
+
   return (
     <div className="space-y-6">
       <form onSubmit={createNote} className="bg-white p-6 rounded-lg shadow-md">
@@ -59,17 +101,17 @@ const NotesClient = ({ intialNotes }) => {
         <div className="space-y-4">
           <input
             type="text"
-            placeholder="Note Title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
+            placeholder="Note Title"
             required
             className="w-full p-3 border border-gray-300 rounded-md focus:outline-none text-gray-800 focus:ring-2 focus:ring-blue-500"
           />
           <textarea
-            placeholder="Note Content"
             className="w-full p-3 border border-gray-300 rounded-md focus:outline-none text-gray-800 focus:ring-2 focus:ring-blue-500"
             value={content}
             onChange={(e) => setContent(e.target.value)}
+            placeholder="Note Content"
             rows={4}
           ></textarea>
           <button
@@ -90,30 +132,72 @@ const NotesClient = ({ intialNotes }) => {
         ) : (
           notes.map((note) => (
             <div key={note._id} className="bg-white p-6 rounded-lg shadow-md">
-              <div className="flex justify-between items-start mb-2 ">
-                <h3 className="text-gray-800 font-semibold text-lg">
-                  {note.title}
-                </h3>
-                <div className="flex gap-2">
-                  <button className="text-blue-500 hover:text-blue-700 text-sm">
-                    Edit
-                  </button>
-                  <button
-                    onClick={() => deletNote(note._id)}
-                    className="text-red-500 hover:text-red-700 text-sm"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-              <p className="text-gray-700 mb-2">{note.content}</p>
-              <p className="text-sm text-gray-500">
-                Created:{new Date(note.createdAt).toLocaleDateString()}
-              </p>
-              {note.updatedAt !== note.createdAt && (
-                <p className="text-sm text-gray-500">
-                  Updated:{new Date(note.updatedAt).toLocaleDateString()}
-                </p>
+              {editingId === note._id ? (
+                <>
+                  {/* Editing mode */}
+                  <div className="space-y-4">
+                    <input
+                      type="text"
+                      value={editTitle}
+                      placeholder="Note Title"
+                      onChange={(e) => setEditTitle(e.target.value)}
+                      required
+                      className="w-full p-3 border border-gray-300 rounded-md focus:outline-none text-gray-800 focus:ring-2 focus:ring-blue-500"
+                    />
+                    <textarea
+                      className="w-full p-3 border border-gray-300 rounded-md focus:outline-none text-gray-800 focus:ring-2 focus:ring-blue-500"
+                      value={editContent}
+                      placeholder="Note Content"
+                      onChange={(e) => setEditContent(e.target.value)}
+                      rows={4}
+                    ></textarea>
+                    <div className="flex gap-2">
+                      <button
+                        onClick={() => updateNode(note._id)}
+                        disabled={loading}
+                        className="bg-green-500 text-white px-6 py-2 rounded-md hover:bg-green-600"
+                      >
+                        {loading ? "Saving..." : "Save"}
+                      </button>
+                      <button
+                        onClick={() => cancelEditing()}
+                        className="bg-gray-500 text-white px-6 py-2 rounded-md hover:bg-gray-600"
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="flex justify-between items-start mb-2 ">
+                    <h3 className="text-gray-800 font-semibold text-lg">
+                      {note.title}
+                    </h3>
+                    <div className="flex gap-2">
+                      <button
+                        onClick={() => startEdiditng(note)}
+                        className="text-blue-500 hover:text-blue-700 text-sm">
+                        Edit
+                      </button>
+                      <button
+                        onClick={() => deletNote(note._id)}
+                        className="text-red-500 hover:text-red-700 text-sm"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                  <p className="text-gray-700 mb-2">{note.content}</p>
+                  <p className="text-sm text-gray-500">
+                    Created:{new Date(note.createdAt).toLocaleDateString()}
+                  </p>
+                  {note.updatedAt !== note.createdAt && (
+                    <p className="text-sm text-gray-500">
+                      Updated:{new Date(note.updatedAt).toLocaleDateString()}
+                    </p>
+                  )}
+                </>
               )}
             </div>
           ))
